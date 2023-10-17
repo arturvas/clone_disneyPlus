1e9c0aa510e6805e948953df5aee0611c6e8a2d8
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2023-10-17 14:45:35 -0400

    add function `imagemin`

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,26 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
 
+// minificador de css
 function styles() {
-  // onde pegaremos os arquivos fontes
   return gulp
     .src("./src/styles/*.scss")
     .pipe(sass({ outputStyle: "compressed" }))
     .pipe(gulp.dest("./dist/css"));
 }
 
-exports.default = styles;
+// minificador de imagens
+function images() {
+  return gulp
+    .src("./src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/images"));
+}
+
+// executa funcao styles e images em paralelo
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function () {
   gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
 };
