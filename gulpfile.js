2651a44fd58b47b495591942c02eaa6d6904f4f8
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2023-10-25 16:05:10 -0400

    uglify para js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
+
+function scripts() {
+  return gulp
+    .src("./src/scripts/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/js"));
+}
 
 // minificador de css
 function styles() {
@@ -19,8 +27,9 @@ function images() {
 }
 
 // executa funcao styles e images em paralelo
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function () {
   gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+  gulp.watch("./src/scripts/*.js", gulp.parallel(scripts));
 };
